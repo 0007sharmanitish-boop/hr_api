b62bf07bf3ca1143f84b29aa970ed24138aeec2c
Nitish Sharma <0007nitishsharma@gmail.com>
2026-05-08 17:39:40 +0530

    Code: For create API

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -5,4 +5,29 @@ class Api::V1::EmployeesController < ApplicationController
       data: employees,
     }, status: :ok
   end
+
+
+  def create
+    employee = Employee.new(employee_params)
+
+    if employee.save
+      render json: {
+        message: "Employee created successfully",
+        data: employee
+      }, status: :created
+    else
+      render json: {
+        errors: employee.errors.full_messages
+      }, status: :unprocessable_content
+    end
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(
+      :first_name, :last_name, :email, :job_title, 
+      :country, :salary, :department, :hire_date, :employee_code
+    )
+  end
+    
 end
