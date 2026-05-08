ae879b6699cc3d5d8516464d3ff841ece1053afe
Nitish Sharma <0007nitishsharma@gmail.com>
2026-05-08 17:39:07 +0530

    Test: for create api

--- spec/requests/api/v1/employees_spec.rb
+++ spec/requests/api/v1/employees_spec.rb
@@ -11,4 +11,47 @@ RSpec.describe "Employees API", type: :request do
       expect(json_body["data"].size).to eq(1)
     end
   end
+
+  describe "POST /api/v1/employees" do
+    context "with valid parameters" do
+      it "creates a new Employee" do
+        expect {
+          post "/api/v1/employees", params: { employee: valid_attributes }
+        }.to change(Employee, :count).by(1)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    
+    context "with invalid parameters" do
+      it "does not create an Employee and returns error" do
+        expect {
+          post "/api/v1/employees", params: { employee: { email: "" } }
+        }.not_to change(Employee, :count)
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+    end
+
+    context "with duplicate data" do
+      it "fails when email already exists" do
+        expect {
+          post "/api/v1/employees", params: { 
+            employee: valid_attributes.merge(email: employee.email) 
+          }
+        }.not_to change(Employee, :count)
+        expect(response).to have_http_status(:unprocessable_content)
+        expect(json_body["errors"]).to include("Email has already been taken")
+      end
+
+      it "fails when employee_code already exists" do
+        expect {
+          post "/api/v1/employees", params: { 
+            employee: valid_attributes.merge(employee_code: employee.employee_code) 
+          }
+        }.not_to change(Employee, :count)
+        expect(response).to have_http_status(:unprocessable_content)
+        expect(json_body["errors"]).to include("Employee code has already been taken")
+      end
+    end
+  end
 end
