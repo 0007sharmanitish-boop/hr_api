ba44753c7653fecbfaf40a68d9c73f5df2082519
Nitish Sharma <0007nitishsharma@gmail.com>
2026-05-09 15:00:07 +0530

    added seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,45 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+puts "Starting Seed Script..."
+start_time = Time.current
+
+first_names = File.readlines('first_names.txt', chomp: true)
+last_names  = File.readlines('last_names.txt', chomp: true)
+
+BATCH_SIZE = 2000
+TOTAL_RECORDS = 10_000
+
+job_titles = ["Software Engineer", "DevOps Engineer", "Data Analyst", "Product manager", "Scrum Master"]
+countries = ["IN", "US", "AU", "CA", "EN"]
+
+(TOTAL_RECORDS/BATCH_SIZE).times do |batch_num|
+  now = Time.current
+  employees = []
+
+  BATCH_SIZE.times do |i|
+    f_name = first_names.sample
+    l_name = last_names.sample
+    country = countries.sample
+    jtitle = job_titles.sample
+
+    employees << {
+      first_name:    f_name,
+      last_name:     l_name,
+      email:         "#{f_name.downcase}.#{l_name.downcase}.#{SecureRandom.hex(4)}@example.com",
+      job_title:     jtitle,
+      country:       country,
+      salary:        rand(60000..150000),
+      department:    "Engineering",
+      hire_date:     Date.today,
+      employee_code: "EMP-#{SecureRandom.hex(6).upcase}",
+      created_at:    now,
+      updated_at:    now
+    }
+  end
+
+  Employee.import(employees, validate: false, timestamps: false) # this is intentionally set to false to make the script faster
+end
+
+duration = Time.current - start_time
+puts "Time taken: #{(duration * 1000).round(2)} ms"
+puts "Done! Total employees: #{Employee.count}" # This line is to test the script ran successfully or not
---
+++ lib/tasks/generate_names.rake
@@ -0,0 +1,12 @@
+namespace :data do
+  desc "Generate source text files for first and last names"
+  task generate_source_files: :environment do
+    # Generate 1000 unique names to provide a large enough pool for 10k combinations
+    File.open('first_names.txt', 'w') { |f| 1000.times { f.puts Faker::Name.unique.first_name } }
+    File.open('last_names.txt', 'w')  { |f| 450.times { f.puts Faker::Name.unique.last_name } }
+    puts "Source files created successfully."
+  end
+end
+
+# before running the seed script, you need to generate the source files first
+# Command to run: docker compose exec hr_api bin/rake data:generate_source_files
